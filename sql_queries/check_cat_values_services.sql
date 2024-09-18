ad15595fc5c7c75579620538f90284f6216fa23a
Luis H Rodríguez <160022546+LuisHRF@users.noreply.github.com>
2024-09-18 16:36:39 +0200

    Updating queries

---
+++ sql_queries/check_cat_values_services.sql
@@ -0,0 +1,4 @@
+-- Check cat values in services
+
+SELECT DISTINCT `Phone Service`, `Multiple Lines`, `Internet Service`
+FROM telco_customer_churn_services;
---
+++ sql_queries/check_customer_id.sql
@@ -0,0 +1,30 @@
+-- Check Customer ID between tables
+
+-- Demo and location
+
+SELECT dem.`Customer ID`
+FROM telco_customer_churn_demographics AS dem
+LEFT JOIN telco_customer_churn_location AS loc
+ON dem.`Customer ID` = loc.`Customer ID`
+WHERE loc.`Customer ID` IS NULL;
+
+-- Demo and services
+SELECT dem.`Customer ID`
+FROM telco_customer_churn_demographics AS dem
+LEFT JOIN telco_customer_churn_services AS ser
+ON dem.`Customer ID` = ser.`Customer ID`
+WHERE ser.`Customer ID` IS NULL;
+
+-- Demo and status
+
+SELECT dem.`Customer ID`
+FROM telco_customer_churn_demographics AS dem
+LEFT JOIN telco_customer_churn_status AS sta
+ON dem.`Customer ID` = sta.`Customer ID`
+WHERE sta.`Customer ID` IS NULL;
+
+-- After checking that all Customer IDs match, we check for any strange values
+
+SELECT `Customer ID`
+FROM telco_customer_churn_demographics
+WHERE `Customer ID` LIKE '% %';
